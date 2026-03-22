2d1fc67f5f5025a99f20650baa0c1951287a4a47
yeseul <joyri0202@gmail.com>
2026-03-22 20:29:30 +0900

    [ori/#7] mission - 감정 우표 클릭 시 글자 색상 변경

--- week1/emotion/app/src/main/java/com/example/emotion/MainActivity.kt
+++ week1/emotion/app/src/main/java/com/example/emotion/MainActivity.kt
@@ -3,18 +3,40 @@ package com.example.emotion
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.emotion.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.happyImg.setOnClickListener {
+            binding.happyText.setTextColor(ContextCompat.getColor(this, R.color.happy))
+        }
+
+        binding.excitedImg.setOnClickListener {
+            binding.excitedText.setTextColor(ContextCompat.getColor(this, R.color.excited))
+        }
+
+        binding.normalImg.setOnClickListener {
+            binding.normalText.setTextColor(ContextCompat.getColor(this, R.color.normal))
+        }
+
+        binding.anxiousImg.setOnClickListener {
+            binding.anxiousText.setTextColor(ContextCompat.getColor(this, R.color.anxious))
+        }
+
+        binding.angerImg.setOnClickListener {
+            binding.angerText.setTextColor(ContextCompat.getColor(this, R.color.anger))
         }
     }
 }
