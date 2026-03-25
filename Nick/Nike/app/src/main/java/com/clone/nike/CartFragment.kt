e7035dc6e837e0accbb0885694b2b33fd984f885
Nick9417 <kimjaehwan0920@gmail.com>
2026-03-25 22:54:05 +0900

    [nick/#10] mission/layout, navgraph 구현

--- Nick/Nike/app/src/main/java/com/clone/nike/CartFragment.kt
+++ Nick/Nike/app/src/main/java/com/clone/nike/CartFragment.kt
@@ -1,4 +1,30 @@
 package com.clone.nike
 
-class CartFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.clone.nike.databinding.FragmentCartBinding
+
+class CartFragment: Fragment() {
+    private lateinit var binding: FragmentCartBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCartBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.cartOrderBT.setOnClickListener {
+            findNavController().navigate()
+        }
+    }
 }
--- Nick/Nike/app/src/main/java/com/clone/nike/PurchaseFragment.kt
+++ Nick/Nike/app/src/main/java/com/clone/nike/PurchaseFragment.kt
@@ -1,4 +1,21 @@
 package com.clone.nike
 
-class PurchaseFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.clone.nike.databinding.FragmentPurchaseBinding
+
+class PurchaseFragment: Fragment() {
+    private lateinit var binding: FragmentPurchaseBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPurchaseBinding.inflate(layoutInflater)
+        return binding.root
+    }
 }
