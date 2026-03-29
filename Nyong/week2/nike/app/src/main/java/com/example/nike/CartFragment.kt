7bf03cdc044e4268c1d7251c63ece129b499bca0
chrry03 <ocr925@dgu.ac.kr>
2026-03-30 00:11:24 +0900

    [nyong/#23] mission: 장바구니->구매하기 구현 완료

--- Nyong/week2/nike/app/src/main/java/com/example/nike/CartFragment.kt
+++ Nyong/week2/nike/app/src/main/java/com/example/nike/CartFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class CartFragment : Fragment() {
 
@@ -15,5 +17,23 @@ class CartFragment : Fragment() {
     ): View? {
         // fragment_cart.xml 이라는 도면을 가져와서 화면에 부풀려라(inflate)!
         return inflater.inflate(R.layout.fragment_cart, container, false)
+    }
+
+    // 화면(뷰)이 완전히 만들어진 직후에 실행되는 함수
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 1. 도화지(view)에서 '주문하기' 버튼을 찾아옵니다.
+        val btnOrder = view.findViewById<Button>(R.id.btn_order)
+
+        // 2. 버튼이 눌렸을 때 할 일을 정해줍니다.
+        btnOrder.setOnClickListener {
+            // 나를 감싸고 있는 부모 스케치북(Activity)을 불러와서,
+            // 그 스케치북에 있는 하단 탭(bottom_nav)을 찾습니다.
+            val bottomNav = requireActivity().findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+            // 하단 탭의 현재 선택된 아이템을 '구매하기(tab_shop)'로 강제로 바꿔버립니다!
+            bottomNav.selectedItemId = R.id.tab_shop
+        }
     }
 }
