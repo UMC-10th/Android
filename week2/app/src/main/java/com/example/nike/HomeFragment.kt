e759d39dc849d5fb014f80c32cc86d01b17c71d9
yeseul <joyri0202@gmail.com>
2026-03-29 22:33:15 +0900

    [ori/#20] mission - Splash 데이터 넘기기

--- week2/app/src/main/java/com/example/nike/HomeFragment.kt
+++ week2/app/src/main/java/com/example/nike/HomeFragment.kt
@@ -5,14 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.nike.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // splash title 받기
+        val titleFromSplash = requireActivity().intent.getStringExtra("title")
+        if (titleFromSplash != null){
+            binding.homeTitle.text = titleFromSplash
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
