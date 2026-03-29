8eb36e38d6ef35a43d723dfb9afdd66e4c7d7524
yeseul <joyri0202@gmail.com>
2026-03-29 22:06:37 +0900

    [ori/#20] mission - 주문하기 버튼 클릭 시 구매하기 탭으로 이동

--- week2/app/src/main/java/com/example/nike/MainActivity.kt
+++ week2/app/src/main/java/com/example/nike/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.nike
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.nike.databinding.ActivityMainBinding
