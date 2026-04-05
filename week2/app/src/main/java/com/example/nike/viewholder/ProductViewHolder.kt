394e2b3044c49c2b18eabbebc3e6fb26ec8c576c
yeseul <joyri0202@gmail.com>
2026-04-05 22:59:33 +0900

    [ori/#31] mission - item_product LinearLayout 변경

--- week2/app/src/main/java/com/example/nike/viewholder/ProductViewHolder.kt
+++ week2/app/src/main/java/com/example/nike/viewholder/ProductViewHolder.kt
@@ -11,30 +11,44 @@ RecyclerView.ViewHolder(binding.root) {
     fun bind(product: ProductData) {
         binding.productImage.setImageResource(product.image)
         binding.productName.text = product.name
-        binding.productDescription.text = product.description
-        binding.productColor.text = product.color
         binding.productPrice.text = product.price
 
-        // 베스트셀러 여부
-        if (product.isBestSeller) {
+        if (product.isBestSeller == true) {
             binding.productBest.visibility = View.VISIBLE
+            binding.spacerBestName.visibility = View.VISIBLE
         } else {
             binding.productBest.visibility = View.GONE
+            binding.spacerBestName.visibility = View.GONE
         }
 
-        // 좋아요
+        if (product.description.isNullOrEmpty()) {
+            binding.productDescription.visibility = View.GONE
+            binding.spacerNameDesc.visibility = View.GONE
+        } else {
+            binding.productDescription.visibility = View.VISIBLE
+            binding.productDescription.text = product.description
+            binding.spacerNameDesc.visibility = View.VISIBLE
+        }
+
+        if (product.color.isNullOrEmpty()) {
+            binding.productColor.visibility = View.GONE
+            binding.spacerDescColor.visibility = View.GONE
+            binding.spacerColorPrice.visibility = View.GONE
+        } else {
+            binding.productColor.visibility = View.VISIBLE
+            binding.productColor.text = product.color
+
+            binding.spacerDescColor.visibility = if (product.description.isNullOrEmpty()) View.GONE else View.VISIBLE
+            binding.spacerColorPrice.visibility = View.VISIBLE
+        }
+
+        // 5. 좋아요
         if (product.isLiked == null) {
             binding.btnLike.visibility = View.GONE
         } else {
             binding.btnLike.visibility = View.VISIBLE
-
-            val heartIcon = if (product.isLiked == true) {
-                R.drawable.ic_like_fill
-            } else {
-                R.drawable.ic_like_empty
-            }
+            val heartIcon = if (product.isLiked == true) R.drawable.ic_like_fill else R.drawable.ic_like_empty
             binding.btnLike.setImageResource(heartIcon)
         }
-
     }
 }
