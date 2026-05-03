6238ad8a7a52e0a7e367bbd4175c7351898602c8
chrry03 <ocr925@dgu.ac.kr>
2026-05-03 23:29:44 +0900

    [nyong/#56] mission: 커밋

--- Nyong/week2/nike/app/build.gradle.kts
+++ Nyong/week2/nike/app/build.gradle.kts
@@ -56,4 +56,17 @@ dependencies {
 
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.viewpager2:viewpager2:1.1.0")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+// OkHttp 로그 확인용
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+// Coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+
+// Glide: URL 이미지 로딩용
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
