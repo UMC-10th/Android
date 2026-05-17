a2af389b76fcb8ecb9ff21124d376b984d006186
yeseul <joyri0202@gmail.com>
2026-05-17 23:48:15 +0900

    [ori/#69] mission - HomeScreen UI 구현

--- week7/app/src/main/java/com/example/nike/presentation/home/HomeScreen.kt
+++ week7/app/src/main/java/com/example/nike/presentation/home/HomeScreen.kt
@@ -1,7 +1,23 @@
 package com.example.nike.presentation.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.nike.R
+import com.example.nike.core.designsystem.theme.NikeTheme
 
 @Composable
 fun HomeRoute(
@@ -16,5 +32,42 @@ fun HomeRoute(
 private fun HomeScreen(
     modifier: Modifier = Modifier,
 ) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(
+                horizontal = 17.dp,
+                vertical = 50.dp
+            )
+    ) {
+        Text (
+            text = "Discover",
+            color = Color.Black,
+            fontSize = 28.sp,
+            modifier = Modifier.padding(start = 24.dp)
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text (
+            text = "9월 4일 목요일",
+            color = Color(0xFF767676),
+            fontSize = 16.sp,
+            modifier = Modifier.padding(start = 24.dp)
+        )
+        Spacer(modifier = Modifier.height(50.dp))
+        Image(
+            painter = painterResource(R.drawable.img_home),
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
 
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeScreenPreview() {
+    NikeTheme {
+        HomeScreen()
+    }
 }
