18d95cfd212ec2e34a1ee7859cdebc3bfc3455ef
Nick9417 <kimjaehwan0920@gmail.com>
2026-05-17 23:48:45 +0900

    [nick/#71] mission/7주차 미션

---
+++ Nick/Nike_compose/app/src/main/java/com/clone/nike_compose/ui/cart/CartScreen.kt
@@ -0,0 +1,77 @@
+package com.clone.nike_compose.ui.cart
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.clone.nike_compose.AppDestination
+import com.clone.nike_compose.R
+import com.clone.nike_compose.ui.home.HomeScreen
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewCartScreen() {
+    MaterialTheme {
+        CartScreen(moveToPurchase = {})
+    }
+}
+
+@Composable
+fun CartScreen(
+    moveToPurchase: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Image(
+                painter = painterResource(R.drawable.icon_bagcircle),
+                contentDescription = "빈 카트 아이콘",
+                modifier = Modifier.size(60.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "장바구니가 비어 있습니다.\n제품을 추가하면 여기에 표시됩니다.",
+                textAlign = TextAlign.Center
+            )
+        }
+
+        Button(
+            onClick = { moveToPurchase() },
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 22.dp)
+        ) {
+            Text("주문하기")
+        }
+    }
+}
