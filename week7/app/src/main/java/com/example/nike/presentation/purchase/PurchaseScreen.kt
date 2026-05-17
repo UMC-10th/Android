596c842b959c83420034d63ffc779c9822a2e12b
yeseul <joyri0202@gmail.com>
2026-05-17 23:50:41 +0900

    [ori/#69] mission - PurchaseScreen UI 구현

--- week7/app/src/main/java/com/example/nike/presentation/purchase/PurchaseScreen.kt
+++ week7/app/src/main/java/com/example/nike/presentation/purchase/PurchaseScreen.kt
@@ -1,7 +1,15 @@
 package com.example.nike.presentation.purchase
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.nike.core.designsystem.theme.NikeTheme
 
 @Composable
 fun PurchaseRoute(
@@ -16,5 +24,22 @@ fun PurchaseRoute(
 private fun PurchaseScreen(
     modifier: Modifier = Modifier,
 ) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = "구매하기 화면",
+            color = Color.Black,
+            fontSize = 20.sp
+        )
+    }
+}
 
+@Preview(showBackground = true)
+@Composable
+private fun PurchaseScreenPreview() {
+    NikeTheme {
+        PurchaseScreen()
+    }
 }
