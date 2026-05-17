b5c18b49de658d08945bc3f61722efa5dba514f0
yeseul <joyri0202@gmail.com>
2026-05-17 23:55:00 +0900

    [ori/#69] WishlistScreen UI 구현

--- week7/app/src/main/java/com/example/nike/presentation/wishlist/WishlistScreen.kt
+++ week7/app/src/main/java/com/example/nike/presentation/wishlist/WishlistScreen.kt
@@ -1,7 +1,17 @@
 package com.example.nike.presentation.wishlist
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.nike.core.designsystem.theme.NikeTheme
 
 @Composable
 fun WishlistRoute(
@@ -16,5 +26,31 @@ fun WishlistRoute(
 private fun WishlistScreen(
     modifier: Modifier = Modifier,
 ) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(
+                top = 28.dp
+            ),
+        contentAlignment = Alignment.TopStart
+    ) {
+        Text(
+            text = "위시리스트",
+            color = Color.Black,
+            fontSize = 28.sp,
+            modifier = Modifier
+                .padding(
+                    vertical = 16.dp,
+                    horizontal = 24.dp
+                )
+        )
+    }
+}
 
+@Preview(showBackground = true)
+@Composable
+private fun WishlistScreenPreview() {
+    NikeTheme {
+        WishlistScreen()
+    }
 }
