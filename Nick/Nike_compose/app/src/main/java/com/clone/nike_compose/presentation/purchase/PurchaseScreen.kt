13b20ccc0bea5f3a62a41def3f6f21c7df49459c
Nick9417 <kimjaehwan0920@gmail.com>
2026-05-24 17:15:22 +0900

    [nick/#81] mission/PurchaseScreen List구현

--- Nick/Nike_compose/app/src/main/java/com/clone/nike_compose/presentation/purchase/PurchaseScreen.kt
+++ Nick/Nike_compose/app/src/main/java/com/clone/nike_compose/presentation/purchase/PurchaseScreen.kt
@@ -1,8 +1,12 @@
 package com.clone.nike_compose.presentation.purchase
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -11,10 +15,16 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.clone.nike_compose.R
 import com.clone.nike_compose.core.data.Goods
 
@@ -28,46 +38,102 @@ fun PreviewPurchaseScreen() {
 
 @Composable
 fun PurchaseScreen(goodsList: List<Goods>) {
-    PurchaseGridList(goodsList)
+    var purchaseGoodsList by remember { mutableStateOf(goodsList) }
+
+    PurchaseGridList(
+        goodsList = purchaseGoodsList,
+        wishOnClick = { goodsId ->
+
+            purchaseGoodsList =
+                purchaseGoodsList.map { goods ->
+
+                    if (goods.goodsId == goodsId) {
+
+                        goods.copy(
+                            isWished = !goods.isWished
+                        )
+
+                    } else {
+                        goods
+                    }
+                }
+        }
+    )
 }
 
 @Composable
-fun PurchaseGridList(goodsList: List<Goods>) {
+fun PurchaseGridList(
+    goodsList: List<Goods>,
+    wishOnClick: (Int) -> Unit,
+) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
         modifier = Modifier.padding(7.dp)
     ) {
         items(goodsList) { goods ->
-            PurchaseGoodsItem(goods)
+            PurchaseGoodsItem(
+                goods,
+                wishOnClick
+            )
         }
     }
 }
 
 @Composable
-fun PurchaseGoodsItem(goods: Goods) {
+fun PurchaseGoodsItem(
+    goods: Goods,
+    wishOnClick: (Int) -> Unit) {
     Column {
-        Image(
-            painter = painterResource(goods.goodsImgResId),
-            contentDescription = goods.goodsName,
-            modifier = Modifier.size(184.dp)
-        )
+        Box {
+            Image(
+                painter = painterResource(goods.goodsImgResId),
+                contentDescription = goods.goodsName,
+                modifier = Modifier.size(184.dp)
+            )
+            Image(
+                painter = painterResource(
+                    if(goods.isWished) {
+                        R.drawable.icon_wish_on
+                    } else {
+                        R.drawable.icon_wish_off
+                    }),
+                contentDescription = "위시 버튼",
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(12.dp)
+                    .size(34.dp)
+                    .clickable {
+                        wishOnClick(goods.goodsId)
+                    }
+            )
+        }
+
         Column(
-            verticalArrangement = Arrangement.spacedBy(5.dp)
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+            modifier = Modifier.padding(horizontal = 14.dp)
         ) {
             Text(
-                text = goods.goodsName
+                text = goods.goodsName,
+                fontSize = 14.sp
             )
 
             Text(
-                text = goods.category
+                text = goods.category,
+                fontSize = 14.sp
             )
 
             Text(
-                text = goods.numberOfColour
+                text = goods.numberOfColour,
+                fontSize = 14.sp
             )
 
             Text(
-                text = goods.goodsPrice
+                text = goods.goodsPrice,
+                fontSize = 14.sp
+            )
+
+            Spacer(
+                modifier = Modifier.height(41.dp)
             )
         }
     }
