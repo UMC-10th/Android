bd02c65dbcc7146ed1fb42a04e3db7658be7c812
Nick9417 <kimjaehwan0920@gmail.com>
2026-05-31 23:43:54 +0900

    [nick/#81] mission/9주차 미션

--- Nick/Nike_compose/app/build.gradle.kts
+++ Nick/Nike_compose/app/build.gradle.kts
@@ -58,4 +58,7 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation("androidx.navigation:navigation-compose:2.9.8")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 }
